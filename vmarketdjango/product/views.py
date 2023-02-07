b530419e23f1462d78a5f5bd08e5e086a393ac43
SarvarVakhobov <sarvarbek0125@gmail.com>
2023-02-07 19:44:47 +0500

    added vue

---
+++ vmarketdjango/product/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import Http404
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from product.models import Product
+from product.serializers import ProductSerializers
+
+class LatestProductsList(APIView):
+    def get(self, request, format=None):
+        product = Product.objects.all()[0:4]
+        seralizer = ProductSerializers(product, many=True)
+        return Response(seralizer.data)
+
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category_slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = product(product)
+        return Response(serializer.data)
--- vmarketdjango/vmarketdjango/asgi.py
+++ vmarketdjango/vmarketdjango/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for vmatketdjango project.
+ASGI config for vmarketdjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vmatketdjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vmarketdjango.settings')
 
 application = get_asgi_application()
